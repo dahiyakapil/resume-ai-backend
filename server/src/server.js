51240faa8e68772d138e7f5e8076a6bcbab3a0fb
KAPIL DAHIYA <kapildahiya309@gmail.com>
2025-08-09 13:35:53 +0530

    changes made..

--- server/src/server.js
+++ server/src/server.js
@@ -24,18 +24,8 @@ const allowedOrigins = [
   "http://localhost:5173"
 ];
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
+app.use(cors({ credentials: true }));
+
 
 // ✅ Session config for prod & dev
 app.use(
